f7d8df45f64f3d374e8529d89d8f19877aba5cc7
liamebre <164887882+liamebre@users.noreply.github.com>
2025-10-31 14:55:09 -0400

    update
    now there are buttons to press that move position when you press it.

--- scripts/finger.gd
+++ scripts/finger.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var score = 0
+var place = 0 
 func _on_pinkey_area_entered(_area: Area2D) -> void:
 	$pinkey.hide()
 
@@ -14,3 +16,24 @@ func _on_point_area_entered(_area: Area2D) -> void:
 
 func _on_thumb_area_entered(_area: Area2D) -> void:
 	$thumb.hide()
+
+
+func _on_button_button_down() -> void:
+	score += 1
+	if score%2 == 0:
+		$Button.global_position = Vector2(1025,575)
+		place +=1
+	else:
+		match place%6:
+			1:
+				$Button.global_position = Vector2(800,450)
+			2:
+				$Button.global_position = Vector2(625,200)
+			3:
+				$Button.global_position = Vector2(375,200)
+			4:
+				$Button.global_position = Vector2(150,325)
+			5:
+				$Button.global_position = Vector2(25,550)
+
+		
--- scripts/main.gd
+++ scripts/main.gd
@@ -30,7 +30,6 @@ func _process(_delta: float) -> void:
 func _getInput():
 	if Input.is_action_just_pressed("lMouse"):
 		$knife.position = get_global_mouse_position()
-		score +=1
 	
 
 func _on_setup_timer_timeout() -> void:
